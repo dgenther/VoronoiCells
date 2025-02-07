3fd2299d4b3f0769ddd2d7c69e7976222b4b563b
dgenther <Protoman00@msn.com>
2025-02-06 18:01:52 -0800

    Still figuring out how to draw the cell regions

--- voronoi.py
+++ voronoi.py
@@ -1,26 +1,34 @@
 from vec import vec2
-from random import randint
+from random import randint, choice, choices
+
+COLORS = ['green', 'red', 'cyan', 'yellow']
 
 class Cell:
-        def __init__(self, pos: vec2):
-            self.pos = pos
-            # self.color = 
+		def __init__(self, pos: vec2):
+			self.pos = pos
+			self.color: str = 'black'
 
 class Voronoi:
-    RESOLUTION: vec2
-    def __init__(self, num_cells: int, resolution: vec2):
-        global RESOLUTION
-        self.num_cells: int = num_cells
-        self.cells: list[Cell] = []
-        RESOLUTION = resolution
-        
+	RESOLUTION: vec2
+	def __init__(self, num_cells: int, resolution: vec2):
+		global RESOLUTION
+		self.num_cells: int = num_cells
+		self.cells: list[Cell] = []
+		RESOLUTION = resolution
+		
 
-    def add_cell(pos:vec2 = None):
-        cell: Cell
+	def add_cell(pos:vec2 = None):
+		cell: Cell
 
-        if not pos:
-            pos_x = randint(0, RESOLUTION.x)
-            pos_y = randint(0, RESOLUTION.y)
-            pos = vec2((pos_x, pos_y))
+		if not pos:
+			pos_x = randint(0, RESOLUTION.x)
+			pos_y = randint(0, RESOLUTION.y)
+			pos = vec2((pos_x, pos_y))
 
-        cell = Cell(pos)
+		cell = Cell(pos)
+
+	def draw(self):
+		cell: Cell
+		for cell in self.cells:
+			if cell:
+				pass
